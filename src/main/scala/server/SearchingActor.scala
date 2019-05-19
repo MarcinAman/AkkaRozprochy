ec22085d531df9f701e43e425f56016a0ab971bc
MarcinAman <marcin.aman@gmail.com>
2019-05-19 09:35:25 +0200

    xd

--- src/main/scala/server/SearchingActor.scala
+++ src/main/scala/server/SearchingActor.scala
@@ -7,6 +7,7 @@ class SearchingActor extends Actor {
   val logger = Logging(context.system, this)
 
   override def receive: Receive = {
+    case SearchRequestRef(value, ref) =>
     case _ => logger.info("Not implemented in searching actor")
   }
 }
